067a1988ee4d4725addd578fee80f1b397992d18
amandeepsingh BAINS <singhamandeep104@yahoo.com>
2026-01-23 04:53:11 -0500

    Move map zoom controls to bottom-right to avoid overlapping with header content.

--- assets/js/script.js
+++ assets/js/script.js
@@ -128,7 +128,13 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         ];
 
-        const map = L.map('map', { scrollWheelZoom: false });
+        const map = L.map('map', {
+            scrollWheelZoom: false,
+            zoomControl: false
+        });
+        L.control.zoom({
+            position: 'bottomright'
+        }).addTo(map);
         // Using Esri World Imagery for premium Satellite/Marine look
         L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
             attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
