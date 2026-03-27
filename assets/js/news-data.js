0484144840dd8a8ee903a9cc275d03f1e9cdbc7b
Enoch <enoch@trideltechnologies.com>
2026-03-27 10:23:24 -0400

    Update linkedin via Admin Panel

--- assets/js/news-data.js
+++ assets/js/news-data.js
@@ -1,6 +1,6 @@
 const NEWS_DATA = [
   {
-    "embedUrl": "https://www.linkedin.com/embed/feed/update/urn:li:activity:7404388049926684672"
+    "embedUrl": "https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7392514859222929409?collapsed=1\" height=\"627\" width=\"504\" frameborder=\"0\" allowfullscreen=\"\" title=\"Embedded post\">"
   },
   {
     "embedUrl": "https://www.linkedin.com/embed/feed/update/urn:li:activity:7404381845112741888"
